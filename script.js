ec1ba65c8ecfbf0946798069b9f311f70e28eb8c
prajna-p-shetty <111850364+prajna-p-shetty@users.noreply.github.com>
2022-09-13 11:39:47 +0530

    Update script.js
    adding thresholds

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+  thresholds: {
+    // 90% of requests must finish within 400ms.
+    http_req_duration: ['p(90) < 400'],
+  },
+};
+
 export default function () {
   http.get('http://64.227.14.236:3000/cqIDP/login');
   sleep(1);
